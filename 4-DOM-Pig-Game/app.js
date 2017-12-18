ad791c3a3e9769fcfb27556b1861e88982883324
Brandon Allred <b32alls@gmail.com>
2017-12-17 21:50:20 -0700

    changes for the new game

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -10,17 +10,7 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector(".dice").style.display = "none";
-
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-
+init ();
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
     //1.Random Number
@@ -42,7 +32,7 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
 
 });
 
-document.querySelector(".btn-hold").addEventListener("click", function (){
+document.querySelector(".btn-hold").addEventListener("click", function () {
     //add current score to global score
     scores[activePlayer] += roundScore;
     //update the UI
@@ -59,7 +49,6 @@ document.querySelector(".btn-hold").addEventListener("click", function (){
         //Next Player
         nextPlayer();
     }
-
 });
 
 function nextPlayer () {
@@ -77,8 +66,26 @@ function nextPlayer () {
     document.querySelector(".player-1-panel").classList.toggle("active");
 
     document.querySelector(".dice").style.display = "none";
-};
 
+}
+
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init () {
+    scrores = [0, 0];
+    activePlayer = 0;
+    activePlayer = 0;
+
+    document.querySelector(".dice").style.display = "none";
+    
+    document.getElementById("score-0").textContent = "0";
+    document.getElementById("score-1").textContent = "0";
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";    
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+    
+}
 
 
 // dice = Math.floor(Math.random() * 6 ) + 1;
