88319f130e2eaf62d1294e16e6dce3f89b69aa0b
Brandon Allred <b32alls@gmail.com>
2017-12-20 16:26:54 -0700

    primatives vs objects and passing a function as a parameter

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -36,7 +36,7 @@ console.log(derek.lastName)
 console.log(jane.lastName)
 console.log(dave.lastName)
 */
-
+/*
 //Object.create
 var personProto = {
     calculateAge: function() {
@@ -55,3 +55,89 @@ var jane = Object.create(personProto,
     yearOfBirth: {value: 1957 },
     job: {value: "Dead"}
 });
+*/
+
+//Primatives vs objects
+/*
+variables are not contained within the object, they are simply a reference.
+
+*/
+
+/*
+//primatives - you can see that each one holds the value b = 23 a = 46
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+//objects all you do is change the reference to the object, you don't actually create a new function it's the same obj.
+var obj1 = {
+    name:"Brandon",
+    age:26
+}
+var obj2 = obj1
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Functions
+var age = 27;
+var obj = {
+    name:"Derek",
+    city: "lehi"
+}
+//When we pass a primative into the function, a copy is created so you can change a. You don't pass an object into
+//the function just a referrence to the function. 
+function change (a,b) {
+    a = 30;
+    b.city = "Draper";
+}
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+/*
+//Passing functions as arguments     "First class functions"
+    - Functions are an instance of the object type.
+    - Behaves like any other object.
+    - store functions in a variable
+    - pass a function as an argument to another function
+    -return a function from a function.
+*/    
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc (arr, fn) {
+    var arrRes = [];
+    for(var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge (el) {
+    return 2017 - el;
+}
+
+function isFullAge(el) {
+    return el>=18;
+}
+
+function maxHeartRate (el) {
+
+    if(el>=18 && el<= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+};
+
+var ages = arrayCalc(years, calculateAge);
+console.log(ages);
+
+var isFullAge = arrayCalc(ages, isFullAge);
+console.log(isFullAge);
+
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(rates);
